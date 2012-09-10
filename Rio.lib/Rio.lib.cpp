511e4e2353822a0e91dbd6b13a6685e5abae666e
John Davis <jdavis@pcprogramming.com>
2012-09-09 21:55:53 -0500

    test code

--- Rio.lib/Rio.lib.cpp
+++ Rio.lib/Rio.lib.cpp
@@ -4,9 +4,13 @@
 #include "stdafx.h"
 
 #include "../include/RIO.lib.h"
+#include <iostream>
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	TUUID test;
+	std::cout << (std::string)test << std::endl;
+
 	return 0;
 }
 
